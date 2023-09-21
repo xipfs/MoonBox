684f9c74d3fb2a86118f6cc50175bf439eabdf5b
0xC000005 <flexie@foxmail.com>
2023-09-21 17:34:27 +0800

    feat(core): 修改判断条件

--- src/main/java/net/xipfs/moonbox/quant/strategy/DoubleEmaStrategy.java
+++ src/main/java/net/xipfs/moonbox/quant/strategy/DoubleEmaStrategy.java
@@ -24,8 +24,8 @@ public class DoubleEmaStrategy{
         ClosePriceIndicator closePrice = new ClosePriceIndicator(series);
         EMAIndicator shortEma = new EMAIndicator(closePrice, 15);
         EMAIndicator longEma = new EMAIndicator(closePrice, 30);
-        Rule entryRule = new OverIndicatorRule(shortEma, longEma);
-        Rule exitRule = new UnderIndicatorRule(shortEma, longEma);
+        Rule entryRule = new CrossedUpIndicatorRule(shortEma,longEma);
+        Rule exitRule = new CrossedDownIndicatorRule(shortEma,longEma);
         return new BaseStrategy(entryRule, exitRule);
     }
 }
